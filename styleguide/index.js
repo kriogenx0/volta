8e0fc311771e4febed4d44a44b672af77e5f050d
Alex Vaos <simplex0@gmail.com>
2026-08-10 11:59:36 -0700

    Make Volta installable as a Git dependency

--- styleguide/index.js
+++ styleguide/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 // Libs
-import "../node_modules/@apple/sf-symbols-web/src/_sf-symbols.scss";
 
 // Load Volta styles
 import "../styles.scss";
